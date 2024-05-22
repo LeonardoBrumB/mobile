a62f916eca561719025e6e12ed44faf9112f1a47
LeonardoBrumB <133942466+LeonardoBrumB@users.noreply.github.com>
2024-05-22 19:01:53 -0300

    commit5

---
+++ mobile/src/AtividadeAvaliativa/Atividade1.tsx
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Pressable, Image, Alert, ScrollView } from 'react-native';
+
+type Props = {
+    titulo: string;
+}
+
+const Atividade1 = (props: Props) => {
+    return (
+        <View style={styles.container}>
+            <View style={styles.container_titulo}>
+                <Text
+                    style={styles.titulo_caixa_texto}>
+                    {props.titulo}
+                </Text>
+                <TextInput
+                    style={styles.caixa_texto}
+                />
+            </View>
+        </View>
+    );
+}
+
+export default Atividade1;
+
+const styles = StyleSheet.create({
+    container: {
+        paddingTop: 250,
+        flex: 1,
+        backgroundColor: '#1d7cd4'
+    },
+    container_titulo: {
+        flex: 2,
+        alignItems: 'center'
+    },
+    titulo_caixa_texto: {
+        fontSize: 25,
+        color: 'black'
+    },
+    caixa_texto: {
+        width: '70%',
+        color: 'black',
+        borderWidth: 1,
+        borderRadius: 4,
+        margin: 3,
+        backgroundColor: 'white'
+    },
+
+});
---
+++ mobile/src/AtividadeAvaliativa/Atividade2.tsx
@@ -0,0 +1,60 @@
+import React, { useState } from 'react';
+import { StyleSheet,View, Image,ScrollView} from 'react-native';
+
+const Atividade2 = () => {
+    const [titulo, setTitulo] = useState('');
+
+
+    return (
+        <ScrollView>
+            <View style={styles.container}>
+                <View style={styles.painel_imagem}>
+                    <Image
+                        style={styles.imagem}
+                        source={{ uri: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png' }} />
+                </View>
+                <View style={styles.painel_imagem}>
+                    <Image
+                        style={styles.imagem}
+                        source={{ uri: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png' }} />
+                </View>
+                <View style={styles.painel_imagem}>
+                    <Image
+                        style={styles.imagem}
+                        source={{ uri: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png' }} />
+                </View>
+                <View style={styles.painel_imagem}>
+                    <Image
+                        style={styles.imagem}
+                        source={{ uri: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png' }} />
+                </View>
+                <View style={styles.painel_imagem}>
+                    <Image
+                        style={styles.imagem}
+                        source={{ uri: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png' }} />
+                </View>
+
+
+            </View>
+        </ScrollView>
+    );
+}
+
+export default Atividade2;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#1d7cd4'
+    },
+    painel_imagem: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    imagem: {
+        width: 200,
+        height: 200,
+        resizeMode: "center"
+    }
+});
